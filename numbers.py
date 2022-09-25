f7c27f0c2fe83a1e0145dc6139391b7c84223695
Jamshid <jallayorov39@gmail.com>
2022-09-25 14:28:36 +0300

    done

---
+++ numbers.py
@@ -0,0 +1,8 @@
+a = int(input("first num"))
+b = int(input("second num"))
+c = int(input("third num"))
+d = int(input("fourth num"))
+
+f= (a+b) / (c+d)
+f=float("{:2f}".format(f))
+print(f)
--- user.py
+++ user.py
@@ -1,1 +1,11 @@
+name = input("What is your name? ")
 
+age = int(input("How old are you? "))
+
+home = input("Where are you live? ")
+
+print("This is",name)
+
+print('Is ' + str(age))
+
+print('She live in',home)
